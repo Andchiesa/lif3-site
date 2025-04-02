c596430254501004af4e13ffca600342ce022c8a
Anderson Chiesa <80640593+Andchiesa@users.noreply.github.com>
2025-04-02 14:19:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -361,7 +361,8 @@ function App() {
                   Ganhe até 10% de bonificação por cada contrato fechado!
                 </p>
                 <p className={`${theme.textSecondary} mt-2`}>
-                  Para se cadastrar, basta selecionar o assunto Indica+ <p className={`${theme.textSecondary} mt-2`}>e enviar sua mensagem para ser direcionado para nossa IA.
+                  Para se cadastrar, basta selecionar o assunto Indica+ </p>
+                  <p>e enviar sua mensagem para ser direcionado para nossa IA.
                 </p>
               </div>
               <button 
